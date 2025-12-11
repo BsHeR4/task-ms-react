40987fe09a8cd96ff2fc1c57be84d27267719da3
BsHeR4 <bshermahayni@gmail.com>
2025-12-11 21:13:34 +0300

    feat: introduce reusable TaskItem component
    - Created `src/components/TaskItem/TaskItem.jsx` to render individual task information within a list.
    - The component accepts `id`, `title`, and `description` as props.
    - Displays the task title (semibold white text) and a truncated description (gray text).
    - Includes two `Link` elements for actions:
      - An Edit button styled with an orange background.
      - A Delete button styled with a red background.
    - Uses Flexbox and Tailwind CSS for layout, ensuring title, description, and action buttons are clearly separated.

---
+++ src/components/TaskItem/TaskItem.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+const TaskItem = ({id, title, description}) => {
+    return (
+        <div key={id} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+                <div className="min-w-0 flex-auto">
+                    <p className="text-sm/6 font-semibold text-white">{title}</p>
+                    <p className="mt-1 truncate text-xs/5 text-gray-400 text-wrap">{description}</p>
+                </div>
+            </div>
+            <div className="flex items-end gap-2">
+                <Link
+                    className="flex justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-orange-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
+                >
+                    Edit
+                </Link>
+                <Link
+                    className="flex justify-center rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500"
+                >
+                    Delete
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default TaskItem
